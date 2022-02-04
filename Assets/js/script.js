fb80ea7b6522518d7f02e403109760430521a5af
Richard Rogers <treyrogers073@gmail.com>
2022-02-03 20:48:23 -0700

    added css color for UV index, and weather response

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -28,28 +28,99 @@ function stageCities(){
     localStorage.setItem('cities', json.stringify(cities))
 }
 
+function rednerCities(){
+    cityList.empty()
 
+    for (let i = 0; i < cities.length; i++) {
+        const city = cities[i];
+        
+        const li = $('<li>').text(city)
+        li.attr('id', 'listC')
+        li.attr('data-city', city)
+        li.attr('class', 'list-group-item')
 
-// search history storage, keeping previous searches in local storage and on
-function storeHistory(event) {
-    event.preventDefault()
-    var history = $(this).children('input').val()
-    cityNames.push(history)
-    localStorage.setItem('data', JSON.stringify(cityNames))
-    buttonRendor()
+        cityList.prepend(li)
+    }
+    if(!city){
+        return
+    } else {
+        weatherResponse(city)
+    }
 }
 
-function buttonRendor() {
-    $('#searchHistory').empty()
-    cityNames.forEach(function (city) {
-        var btnEl = $('<button>')
-        btnEl.text(city)
-        $('#searchHistory').append(btnEl)
-        console.log(btnEl)
+$('#add-city').on('click', (event)=>{event.preventdefault()
+
+const city = $('city-input').val().trim()
+
+if(city===''){
+    return
+}
+cities.push(city)
+
+renderCities()
+stageCities()
+});
+
+//get weather response
+
+function weatherResponse(name){
+    const query = "https://api.openweathermap.org/data/2.5/weather?q=" +name+ "&appid=" + apiKey;
+
+    $('#today-weather').empty()
+    $.ajax({
+        url: query,
+        method: 'GET'
+    })
+    .then(function(){
+        title = $('<h3>').text(response.name+''+formatDay())
+        $('#today-weather').append(cityTitle)
+
+        const tempToNum = parseInt((response.main.temp)*9/5-459)
+
+        const cityTemp = $('<p>').text('Temperature:'+ tempToNum + 'f')
+        $('today-weather').append(cityTemp)
+
+        const humidity = $('<p>').text('Humidity:'+ response.main.humidity + '%')
+        $('today-weather').append(humidity)
+
+        const windSpeed = $('<p>').text('Wind Speed:' + response.wind.speed + 'mph')
+        $('today-weather').append(windSpeed)
+
+        const long = response.coord.lon
+        const lat = response.coord.lat
+
+        const queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid="+ key+ "&lat=" + lat +"&lon=" + long
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        
+        .then(function(resUV){
+            const UV = $('<span>').text(resUV.value)
+            const UVp = $('<p>').text("UVIndex:")
+
+            UVp.append(UV)
+
+            $('today-weather').append(UVp)
+
+            if(resUV.value > 0 && resUV.value <=2){
+                UV.attr('class', 'green')
+            }else if (resUV.value > 2 && resUV.value <= 5){
+                UV.attr("class","yellow")
+            }
+            else if (resUV.value >5 && resUV.value <= 7){
+                UV.attr("class","orange")
+            }
+            else if (resUV.value >7 && resUV.value <= 10){
+                UV.attr("class","red")
+            }
+            else{
+                UV.attr("class","purple")
+            }
+        });
+
+
     })
 }
 
-
-buttonRendor()
-
-$('#searchForm').submit(storeHistory)
